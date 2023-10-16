2e41e35a8aa2bfa93228b422d406abfa30148772
zouyongsheng <zouyongsheng@kuaishou.com>
2023-10-16 14:46:21 +0800

    feat: 更新

--- sync_offline.js
+++ sync_offline.js
@@ -34,7 +34,7 @@ const filterFileName = [
   'package-lock.json',
   'yarn.lock',
   'pages',
-  'mybricks-app-pcspa.zip',
+  'mybricks-app-th5',
   '.idea',
   '.git',
   '.vscode',
@@ -44,7 +44,7 @@ const filterFileName = [
 ];
 
 const files = fs.readdirSync(zipDirPath).filter(filename => {
-  return !filterFileName.includes(filename);
+  return filterFileName.indexOf(filename) === -1;
 });
 
 read(rootDir, files, zipDirPath);
@@ -56,6 +56,6 @@ zip.generateAsync({
     level: 9
   }
 }).then((content) => {
-  fs.writeFileSync(path.join(__dirname, `./${packageJSON.name}.zip`), content, 'utf-8');
+  fs.writeFileSync(path.join(__dirname, `./${packageJSON.name}-${Date.now()}.zip`), content, 'utf-8');
   console.log(`离线包打包完成，请将 ${packageJSON.name}.zip 拖拽到平台进行离线安装`);
 });
